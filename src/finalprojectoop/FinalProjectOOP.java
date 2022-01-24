a1edf9de540e9f8b21a6f580cdfde1344c315d22
dirgaastya <astya444@gmail.com>
2022-01-24 09:00:37 +0700

    Transaction App

---
+++ src/finalprojectoop/FinalProjectOOP.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectoop;
+import java.sql.*;
+
+/**
+ *
+ * @author Taufik Yahya
+ */
+public class FinalProjectOOP {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        Login newLogin = new Login();
+        newLogin.setVisible(true);
+//        connect newcon = new connect();
+//        newcon.getConnection();
+    }
+    
+}
---
+++ src/finalprojectoop/connect.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectoop;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Taufik Yahya
+ */
+public class connect {
+    private static  Connection connect;
+    
+    public static Connection getConnection() throws SQLException{
+        try {
+            String DB ="jdbc:mysql://localhost/user_db";
+            String user="root";
+            String pass="root";
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            connect = (Connection) DriverManager.getConnection(DB,user,pass);
+            System.out.println("connect");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Tidak ada koneksi","Error",
+            JOptionPane.INFORMATION_MESSAGE);
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+        return connect;
+    } 
+}
+
